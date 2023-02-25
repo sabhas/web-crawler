9a87dda14c8a20af50829dc8016436d3eb10d83b
Sabir Hassan <sabirbk06@gmail.com>
2023-02-26 02:58:22 +0500

    basic implementation of crawler

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello world')
+import { crawlPage } from './crawl'
+import { printReport } from './report'
+import { getUrl } from '@sasjs/utils/input'
+
+const main = async () => {
+  const baseURL = await getUrl('Please provide the URL of the website you want to crawl', 'Invalid url is provided!')
+
+  console.log(`starting crawl of: ${baseURL}...`)
+
+  const pages = await crawlPage(baseURL, baseURL, {})
+
+  printReport(pages)
+}
+
+main()
---
+++ src/report.ts
@@ -0,0 +1,28 @@
+// printReport takes a dictionary of pages and prints them
+// to the console in a human-friendly way
+export const printReport = (pages: { [key: string]: number }) => {
+  console.log('==========')
+  console.log('REPORT')
+  console.log('==========')
+
+  const sortedPages = sortPages(pages)
+
+  for (const sortedPage of sortedPages) {
+    const url = sortedPage[0]
+    const count = sortedPage[1]
+    console.log(`Found ${count} internal links to ${url}`)
+  }
+}
+
+// sortPages sorts a dictionary of pages
+// into a list of tuples (url, count)
+// with the highest counts first in the list
+function sortPages(pages: { [key: string]: number }) {
+  // 2D array where the
+  // inner array: [ url, count ]
+  const pagesArr = Object.entries(pages)
+  pagesArr.sort((pageA, pageB) => {
+    return pageB[1] - pageA[1]
+  })
+  return pagesArr
+}
